f6a77668b32c1f1b652312d1c81d6d9c85cd16e0
Dini4ka <boss.karelov@gmail.com>
2022-11-15 00:39:21 +0300

    Организация работы черезе VPN

--- VPN/get_port.py
+++ VPN/get_port.py
@@ -2,11 +2,12 @@ import random
 
 
 def get_port():
-    switch = random.randint(1, 3)
+    switch = random.randint(0, 2)
     result = dict(login='iharmikhailovich1341', password='e8c14a')
-    with open('ports.txt') as f:
-        ip = f.readlines(switch)[0].split('@')[1].split(':')[0]
-        port = f.readlines(switch)[0].split('@')[1].split(':')[1][:-1]
+    with open('../start/ports.txt') as f:
+        ip_and_port = f.readlines(0)[switch].split('@')[1].split(':')
+        ip = ip_and_port[0]
+        port = ip_and_port[1][:-1]
         result.update({('ip', ip), ('port', port)})
         f.close()
     return result
