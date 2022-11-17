1a4f23c933adf4ec46471106d7271c216bf655a8
Dini4ka <boss.karelov@gmail.com>
2022-11-17 21:56:07 +0300

    Проверка работы всех прокси серверов

--- start/main.py
+++ start/main.py
@@ -2,24 +2,20 @@ from webdriver_manager.chrome import ChromeDriverManager
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from extension import proxies
-from VPN import get_port
+from VPN import get_all_ports
 from time import sleep
 
-username = 'iharmikhailovich1341'
-password = 'e8c14a'
-endpoint = '109.248.7.195'
-port = '10181'
 website = 'https://2ip.ru'
+
+proxy_list = get_all_ports()
 
 chrome_options = webdriver.ChromeOptions()
 
-proxies_extension = proxies(username, password, endpoint, port)
-
-chrome_options.add_extension(proxies_extension)
-#chrome_options.add_argument("--headless=chrome")
-
-
-chrome = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=chrome_options)
-chrome.get(website)
-sleep(10)
-chrome.close()
+for proxy in proxy_list:
+    proxies_extension = proxies(proxy['login'], proxy['password'], proxy['ip'], proxy['port'])
+    chrome_options.add_extension(proxies_extension)
+    #chrome_options.add_argument("--headless=chrome")
+    chrome = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=chrome_options)
+    chrome.get(website)
+    sleep(10)
+    chrome.close()
