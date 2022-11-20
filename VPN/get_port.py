e8d9b5c3734ef938e926a6d8ca90c0c36050a0dc
Dini4ka <boss.karelov@gmail.com>
2022-11-20 22:35:13 +0300

    полная настройка работы с proxy, обход защиты cloudfare и добавление утилиты Display для успешной работы софта на сервере Tls Ubuntu

--- VPN/get_port.py
+++ VPN/get_port.py
@@ -4,7 +4,7 @@ import random
 def get_port():
     switch = random.randint(0, 2)
     result = dict(login='iharmikhailovich1341', password='e8c14a')
-    with open('../start/ports.txt') as f:
+    with open('../settings/ports.txt') as f:
         ip_and_port = f.readlines(0)[switch].split('@')[1].split(':')
         ip = ip_and_port[0]
         port = ip_and_port[1][:-1]
@@ -15,7 +15,7 @@ def get_port():
 
 def get_all_ports():
     result = []
-    with open('../start/ports.txt') as f:
+    with open('../settings/ports.txt') as f:
         for line in f:
             a = dict(login='iharmikhailovich1341', password='e8c14a')
             ip_and_port = line.split('@')[1].split(':')
